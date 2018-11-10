d60ed4a887e8d47dec0a158ee77e1ac2190d497c
Joe J Collins <joejcollins@gmail.com>
2018-11-10 19:03:50 +0000

    Maybe

--- web_app/index.php
+++ web_app/index.php
@@ -5,5 +5,12 @@
  <body>
  <?php echo '<p>Hello World</p>'; ?> 
  <?php echo '<p>'.getcwd().'</p>'; ?> 
+ <?php
+$myfile = fopen("./data/data.txt", "r") or die("Unable to open file!");
+
+
+echo fread($myfile,filesize("./data/data.txt"));
+fclose($myfile);
+?>
  </body>
 </html>
