80b1e25b20be47dc7ba9a0cd018c86a606d179a8
Joe J Collins <joejcollins@gmail.com>
2018-11-10 21:51:40 +0000

    sqlite test

--- web_app/index.php
+++ web_app/index.php
@@ -7,10 +7,21 @@
  <?php echo '<p>'.getcwd().'</p>'; ?> 
  <?php
 $myfile = fopen("./data/data.txt", "r") or die("Unable to open file!");
-
-
 echo fread($myfile,filesize("./data/data.txt"));
 fclose($myfile);
 ?>
+<?php
+
+$dir = './data/data.sqlite';
+$dbh  = new PDO($dir) or die("cannot open the database");
+$query =  "select * from tbl1";
+foreach ($dbh->query($query) as $row)
+{
+    echo $row[0];
+}
+$dbh = null;
+?>
+
+
  </body>
 </html>
