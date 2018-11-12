7056252e7e0dcee2aabde42a1226bfdc088a97b9
Joe J Collins <joejcollins@gmail.com>
2018-11-12 14:53:42 +0000

    qwer

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
 <h1>SQLite</h1>
 <?php
-$dir = 'sqlite3:./data/data.sqlite';
+$dir = 'sqlite:data/data.sqlite';
 $dbh = new PDO($dir) or die("cannot open the database");
 $query = "select * from tbl1";
 foreach ($dbh->query($query) as $row) {
