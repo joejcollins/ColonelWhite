ecdc47931acfa097ac4d685f03c271cea2ac13b2
Joe J Collins <joejcollins@gmail.com>
2018-11-15 11:50:14 +0000

    Swapped auto and index.php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-    require 'auto';
+// auto is the entry point for GAE applications
+switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
+    case '/':
+        require 'data.php';
+        break;
+    case '/phpinfo.php':
+        require 'phpinfo.php';
+        break;
+    default:
+        http_response_code(404);
+        exit('Not Found');
+}
 ?>
